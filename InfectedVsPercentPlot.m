f9ab0b174df10442e89e97ca667572c018ef7ee3
cyyongoueth <31543342+cyyongoueth@users.noreply.github.com>
2019-12-07 15:32:19 -0600

    Create InfectedVsPercentPlot.m

---
+++ InfectedVsPercentPlot.m
@@ -0,0 +1,7 @@
+% Amount Infected vs. Inoculated Percentage 
+
+plot(Inoculated,Infected,'g--o','LineWidth',1); 
+
+title('Infected vs. Inoculated '); 
+xlabel('Inocaluted (%)');
+ylabel('Peak Infected'); 
